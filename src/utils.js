6c9f2ad7fd984ca50d6ddff03f7432e9b06bdf05
Pereverziev Andrii <pereverziev.andrii@gmail.com>
2020-03-11 15:35:52 +0200

    sendMail Function with Nodemailer

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,34 @@
+import dotenv from "dotenv"
+import path from "path"
+dotenv.config({path: path.resolve(__dirname, ".env")})
+
 import { adjectives } from './words/adjectives';
 import { nouns } from './words/nouns';
+import nodemailer from "nodemailer";
+import sgTransport from 'nodemailer-sendgrid-transport';
 
 export const generateSecret = () => {
     const randomNumber = Math.floor(Math.random() * adjectives.length);
     return `${adjectives[randomNumber]} ${nouns[randomNumber]}`;
+}
+
+export const sendMail = (email) => {
+    const options = {
+        auth: {
+            api_user: process.env.SENDGRID_USERNAME,
+            api_key: process.env.SENDGRID_PASSWORD
+        }
+    }
+    const client = nodemailer.createTransport(sgTransport(options));
+    return client.sendMail(email);
+}
+
+export const sendSecretMail = (address, secret) => {
+    const email = {
+        from: "y4t6official@gmail.com",
+        to: address,
+        subject: "Login Secret for Prismagram :)",
+        html: `Hello! Your login secret is <b>${secret}</b><br/>`
+    };
+    sendMail(email);
 }
